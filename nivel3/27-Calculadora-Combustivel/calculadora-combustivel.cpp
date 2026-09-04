13ecda0d1ed61b2eb0f9432d66c50a1232b73411
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-04 19:33:33 -0300

    mod - calculadora combustivel

--- nivel3/27-Calculadora-Combustivel/calculadora-combustivel.cpp
+++ nivel3/27-Calculadora-Combustivel/calculadora-combustivel.cpp
@@ -7,8 +7,20 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
-    cout << "Hello World";
+    double km = 0.00, vlr_gas = 0.00, consumo = 0.00;
 
+    cout << "Valor do combustível: R$";
+    cin >> vlr_gas;
+
+    cout << "Distância (KM): ";
+    cin >> km;
+
+    cout << "Consumo (KM/L): ";
+    cin >> consumo;
+
+    cout << "Combustível necessário: " << km/consumo << "\nValor total: " << (km/consumo) * vlr_gas <<  endl;
+
+    cin.ignore();
     getchar();
     return 0;
 }
