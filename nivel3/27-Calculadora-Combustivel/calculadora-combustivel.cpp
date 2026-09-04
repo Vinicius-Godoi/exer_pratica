0300ff2fbfe16a271c42a17ff9e0bb91d6c21aab
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-04 17:35:16 -0300

    add - Calculadora Combustivel

---
+++ nivel3/27-Calculadora-Combustivel/calculadora-combustivel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    cout << "Hello World";
+
+    getchar();
+    return 0;
+}
