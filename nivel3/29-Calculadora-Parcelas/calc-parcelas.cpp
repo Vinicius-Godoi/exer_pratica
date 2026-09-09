9915f44c461ee2b82552dfa9abe31ffda8b3ec33
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-09 16:14:35 -0300

    update - Adição da escolha do modo de pagamento e otimização geral

--- nivel3/29-Calculadora-Parcelas/calc-parcelas.cpp
+++ nivel3/29-Calculadora-Parcelas/calc-parcelas.cpp
@@ -22,12 +22,24 @@ int main() {
     cout << "Valor da compra: R$";
     cin >> compra;
 
-    cout << "MAX parcelas: ";
-    cin >> nParc;
+    char pag;
+    cout << "\nQual a forma de pagamento?\n 1- À Vista  2- Parcelado" << endl;
+    cin >> pag;
+    
+    switch (pag){
+        case '1':
+            cout << "\nÀ VISTA: " << endl;
 
-    cout << fixed << setprecision(2);
-    cout << "\n\nPARCELAMENTO: \n";
-    calcParc(compra, nParc);
+            cout << "VALOR TOTAL: R$" << compra - (compra*0.10) << " [10% de desconto]" << endl;
+            break;
+        case '2':
+            cout << "\nPARCELAMENTO: " << endl;
+            cout << "MAX parcelas: ";
+            cin >> nParc;
+           
+            calcParc(compra, nParc);
+    }
+    
 
     cin.ignore();
     getchar();
