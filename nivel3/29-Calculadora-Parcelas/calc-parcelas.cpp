3a17a329ec625083fb97b3b5ce593814a508b726
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-09 16:01:49 -0300

    update - Otimização da função calcParc(). Inlcusão da opção de números de parcelas

--- nivel3/29-Calculadora-Parcelas/calc-parcelas.cpp
+++ nivel3/29-Calculadora-Parcelas/calc-parcelas.cpp
@@ -4,37 +4,30 @@
 
 using namespace std;
 
-void calcParc(double vlr){
-    cout << "12x R$" << vlr/12 << endl 
-         << "11x R$" << vlr/11 << endl 
-         << "10x R$" << vlr/10 << endl 
-         << "9x R$" << vlr/9 << endl 
-         << "8x R$" << vlr/8 << endl
-         << "7x R$" << vlr/7 << endl
-         << "6x R$" << vlr/6 << endl
-         << "5x R$" << vlr/5 << endl
-         << "4x R$" << vlr/4 << endl
-         << "3x R$" << vlr/3 << endl
-         << "2x R$" << vlr/2 << endl;
+void calcParc(double vlr, int parc){
+    cout << fixed << setprecision(2);
+
+    for(int i = parc; i >=2; --i){
+        cout << i << "x R$" << vlr/i << '\n';
+    }
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
-    double vlrCompra = 0;
+    double compra = 0;
+    int nParc = 0;
 
     cout << "Valor da compra: R$";
-    cin >> vlrCompra;
+    cin >> compra;
 
-    /*cout << "\nNúmero de parcelas: ";
-    cin >> nParcelas;*/
-
-    //vlrParcela  calcParc(vlrCompra, nParcelas);
+    cout << "MAX parcelas: ";
+    cin >> nParc;
 
     cout << fixed << setprecision(2);
     cout << "\n\nPARCELAMENTO: \n";
-    calcParc(vlrCompra);
+    calcParc(compra, nParc);
 
     cin.ignore();
     getchar();
