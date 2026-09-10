257065d2f02c6a96122495d8da3e1c9d8107182b
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-10 19:31:36 -0300

    add - Implementação da solicitação de tipo de medida no conversor de medidas

--- nivel3/33-Conversor-Medidas/conversor-medidas.cpp
+++ nivel3/33-Conversor-Medidas/conversor-medidas.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <windows.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -7,5 +9,21 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
+    char opc;
+    double cm = 0, mt = 0, km = 0;
+
+    const vector<string> medida =
+    {
+        "Centímetros",
+        "Metros",
+        "Quilometros"
+    };
+
+    cout << "Qual tipo de medida irá inserir?";
+    for(size_t i = 0; i <= medida.size(); ++i)
+    {
+        cout << 1+i << " - " << medida[i] << endl;
+    }
+
     return 0;
 }
