5f0dd3310df232bb0995594034f53942a90e387f
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-12 18:57:51 -0300

    add - Correção na lógica de conversão de medidas e atualização do executável

--- nivel3/33-Conversor-Medidas/conversor-medidas.cpp
+++ nivel3/33-Conversor-Medidas/conversor-medidas.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 using namespace std;
-
+// Um programa que converte medidas. O usuário entra com uma medida e fazemos a conversão dela para outras 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
@@ -12,18 +12,54 @@ int main() {
     char opc;
     double cm = 0, mt = 0, km = 0;
 
-    const vector<string> medida =
-    {
+    const vector<string> medida = {
         "Centímetros",
         "Metros",
         "Quilometros"
     };
 
-    cout << "Qual tipo de medida irá inserir?";
-    for(size_t i = 0; i <= medida.size(); ++i)
-    {
-        cout << 1+i << " - " << medida[i] << endl;
+    cout << "Qual tipo de medida irá inserir?" << endl;
+    for(size_t i = 0; i < medida.size(); ++i)
+    { cout << 1+i << " - " << medida[i] << endl; }
+    cin >> opc;
+
+    switch(opc){
+        case '1':
+            cout << "\nCentímetros: ";
+            cin >> cm;
+
+            mt = cm/100;
+            km = cm/100000;
+
+            cout << "\nMetros: " << mt << "\nQuilômetros: " << km;
+
+            break;
+        
+        case '2':
+            cout << "\nMetros: ";
+            cin >> mt;
+
+            cm = mt*100;
+            km = mt/1000;
+
+            cout << "\nCentímetros: " << cm << "\nQuilômetros: " << km;
+
+            break;
+
+        case '3':
+            cout << "\nQuilômetros: ";
+            cin >> km;
+
+            cm = km * 100000;
+            mt = km * 1000;
+
+            cout << "\nCentímetros: " << cm << "\nMetros: " << mt;
+
+            break;
     }
 
+
+    cin.ignore();
+    getchar();
     return 0;
 }
