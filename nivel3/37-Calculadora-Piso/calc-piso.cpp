abb720c750acfaf62d8790a16c4610d866d81434
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-15 16:20:56 -0300

    add - Implementação da lógica de cálculo de pisos e geração do executável

--- nivel3/37-Calculadora-Piso/calc-piso.cpp
+++ nivel3/37-Calculadora-Piso/calc-piso.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <windows.h>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -12,7 +14,37 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
+    // peça p/ caixa
+    int pCx = 0;
+    // largura sala, comprimento sala, largura piso, comprimento piso
+    double lS = 0, cS = 0, lP = 0, cP = 0;
 
+    cout << "Largura  e comprimento da sala (m): ";
+    cin >> lS >> cS;
 
+    cout << "\nLargura  e comprimento do piso (cm): ";
+    cin >> lP >> cP;
+
+    // área sala + 10% de sobra
+    double aS = (lS * cS) * 1.10;
+    // área piso
+    double aP = (lP / 100) * (cP / 100);
+    // total piso
+    double totP = aS / aP;
+
+    cout << "\nQtd peças p/ caixa: ";
+    cin >> pCx;
+
+    // total caixa
+    // 'ceil' arredonda o valor fracionado para cima
+    int totCx = ceil(totP / pCx);
+
+    cout << fixed << setprecision(2);
+    cout << "\n\nR•E•S•U•M•O\nÁrea sala: " << aS << "m²"
+        << "\nQtd peças necessárias: " << totP
+        << "\nQtd caixas fechadas: " << totCx;
+    
+    cin.ignore();
+    getchar();
     return 0;
 }
