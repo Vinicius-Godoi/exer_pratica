54a14694d10c46fa6b781d6024728dd7318cf7a7
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-15 15:36:04 -0300

    add - Implementação da calculadora de pisos necessários para cobrir uma sala

---
+++ nivel3/37-Calculadora-Piso/calc-piso.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+// Programa que calcula a quantidade de pisos necessários para cobrir uma sala, sem desperdícios.
+// ENTRADAS: Largura(sala), Comprimento(sala), Largura(piso), Comprimento(piso), Peças p/ caixa.
+// CÁLCULO: área(sala), área(piso), total peças, total caixas.
+// SAÍDA: área(sala), quantidade peças necessárias, quantidade de caixas fechadas.
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+
+
+    return 0;
+}
