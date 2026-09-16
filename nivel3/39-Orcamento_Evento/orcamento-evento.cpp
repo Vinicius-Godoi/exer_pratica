69d792f36df64e36976605ab0eac4424b2a66e8f
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-15 21:21:39 -0300

    add - Implementação da lógica de cálculo de custo de evento e geração do executável

--- nivel3/39-Orcamento_Evento/orcamento-evento.cpp
+++ nivel3/39-Orcamento_Evento/orcamento-evento.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,5 +8,37 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
     
+    int qPerson = 0;
+    double cPerson = 0;
+
+    while(true)
+    {
+        cout << "Quantidade de pessoas: ";
+        cin >> qPerson;
+        if (!qPerson)
+        {
+            cout << "É necessário entrar com um número inteiro!" << endl;
+            continue;
+        }
+
+        cout << "Custo por pessoa: R$";
+        cin >> cPerson;
+        if (!cPerson)
+        {
+            cout << "É necessário entrar com um número!" << endl;
+            continue;
+        }
+
+        double cEvent = qPerson * cPerson;
+
+        cout << fixed << setprecision(2) << endl;
+        cout << "\nCusto do evento: R$" << cEvent;
+
+        break;
+    }
+    
+
+    cin.ignore();
+    getchar();
     return 0;
 }
