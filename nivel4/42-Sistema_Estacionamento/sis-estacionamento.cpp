8bc4e824dea1ed3c7e6c097bba2e19ebbc3fdd7f
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-16 18:04:17 -0300

    add - Implementação da base do sistema de estacionamento com cálculo de tempo e valor a pagar

---
+++ nivel4/42-Sistema_Estacionamento/sis-estacionamento.cpp
@@ -0,0 +1,19 @@
+/*O usuário informa: 
+    • horário de entrada;  
+    • horário de saída;  
+    • tipo de veículo.  
+O programa calcula o tempo de permanência e o valor a pagar.*/
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    
+    
+    return 0;
+}
