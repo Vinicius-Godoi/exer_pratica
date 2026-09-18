a81c03b443fca82e42ae0fbadecf01f09cfc1fbf
dev_ViniGD <vinicius03godoi@gmail.com>
2026-09-18 20:17:00 -0300

    add - Adição do arquivo executável do sistema de estacionamento e implementação do código-fonte

---
+++ nivel4/42-Sistema_Estacionamento/output/sis-estacionamento.cpp
@@ -0,0 +1,89 @@
+/*O usuário informa: 
+    • horário de entrada;  
+    • horário de saída;  
+    • tipo de veículo.  
+O programa calcula o tempo de permanência e o valor a pagar.*/
+
+#include <iostream>
+#include <windows.h>
+#include <format>
+#include <chrono>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+auto dadosHora()
+{
+    int hora = 0, min = 0, seg = 0;
+
+    cout << "Hora: ";
+    cin >> hora;
+    hours horas{hora};
+    
+    cout << "Minutos: ";
+    cin >> min;
+    minutes minutos{min};
+    
+    cout << "Segundos: ";
+    cin >> seg;
+    seconds segundos{seg};
+
+    auto horaFinal = horas + minutos + segundos;
+
+    return horaFinal;
+}
+
+auto convertHora(int hora, int minuto, int segundo)
+{
+    hours hr_input{hora};
+    minutes min_input{minuto};
+    seconds seg_input{segundo};
+
+    auto convertido = hr_input + min_input + seg_input;
+
+    return convertido;
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    
+    while(true)
+    {
+        const vector<string> modelos = {"Carro popular", "SUV", "Camionete", "Moto"};
+
+        // Entrada
+        auto horaEntrada = dadosHora();
+        cout << "Horário de entrada: " << format("{:%H:%M:%S}", horaEntrada) << endl;
+
+        // Saída
+        auto horaSaida = dadosHora();
+        cout << "Horário de entrada: " << format("{:%H:%M:%S}", horaSaida) << endl;
+
+        cout << "Tipo de veículo:" << endl;
+        for(size_t i = 0; i < modelos.size(); i++){cout << 1 + i << " - " << modelos[i] << endl;}
+        int opc;
+        cin >> opc;
+
+
+
+        /*switch(opc)
+            case '1':*/
+
+
+
+        char repeat;
+        cout << "Deseja repetir? (Sim = 's' | Não - [QUALQUER TECLA]) - ";
+        cin >> repeat;
+
+        if(repeat == tolower('s')){cout << '\n' << endl; continue;}
+        
+        break;
+    }
+
+    cin.ignore();
+    getchar();
+    return 0;
+}
